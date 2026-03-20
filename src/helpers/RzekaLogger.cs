9f11bf6f957661aa3891a3c5623bd5b272ce7bec
kimja <inthenameoferis@gmail.com>
2026-03-20 13:02:39 +0100

    Removed old net rx and unirx sourcefiles since that won't be necessary for godot.

---
+++ src/helpers/RzekaLogger.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public static class RzekaLogger
+{
+    public static void Log(Exception ex)
+    {
+        // https://answers.unity.com/questions/41403/open-script-editor-ide-to-a-particular-linefunctio.html
+        Regex regex = new Regex(@"Assets.+\.cs:\d+");
+        var matches = regex.Matches(ex.StackTrace);
+        StringBuilder sb = new StringBuilder();
+        foreach (var item in matches)
+        {
+            sb.AppendLine($"{item.ToString()}");
+        }
+        Debug.LogError($"Message: {ex.Message}\nStack Trace: {sb.ToString()}");
+    }
+}
